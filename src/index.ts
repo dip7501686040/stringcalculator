50c4d5d36867642586c2eeacbfc55b44c2fa6364
dip7501686040 <dip7501686040@gmail.com>
2024-10-20 18:33:35 +0530

    Added test for using custom delimiter, Implemented functions to handle custom delimiter

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,30 @@
-const stringCalculator = {
-  add: (input: string): number => {
-    const numbers = input.split(/\n|,/).map(Number)
+type StringCalculator = {
+  calculate: (input: string) => number
+  add: (numbers: number[]) => number
+  hasCustomDelimiter: (input: string) => boolean
+  getDelimiter: (input: string) => string
+  getNumbers: (input: string) => number[]
+}
+
+const stringCalculator: StringCalculator = {
+  calculate: function (input: string): number {
+    if (this.hasCustomDelimiter(input)) {
+      return this.add(this.getNumbers(input))
+    }
+    const numbers = input.split(/\n|,/g).map(Number)
+    return this.add(numbers)
+  },
+  add: function (numbers: number[]): number {
     return numbers.reduce((total, num) => total + num, 0)
+  },
+  hasCustomDelimiter: function (input: string): boolean {
+    return input.startsWith("//")
+  },
+  getDelimiter: function (input: string): string {
+    return input.split("\n")[0].split("//")[1]
+  },
+  getNumbers: function (input: string): number[] {
+    return input.split("\n")[1].split(this.getDelimiter(input)).map(Number)
   }
 }
 export default stringCalculator
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,22 +2,26 @@ import stringCalculator from "../src/index"
 
 describe("String Calculator", () => {
   test("should return 0 for an empty string", () => {
-    expect(stringCalculator.add("")).toBe(0)
+    expect(stringCalculator.calculate("")).toBe(0)
   })
 
   test("should return the number for a single number", () => {
-    expect(stringCalculator.add("1")).toBe(1)
+    expect(stringCalculator.calculate("1")).toBe(1)
   })
 
   test("should return the sum of two numbers", () => {
-    expect(stringCalculator.add("2,3")).toBe(5)
+    expect(stringCalculator.calculate("2,3")).toBe(5)
   })
 
   test("should return the sum of multiple numbers", () => {
-    expect(stringCalculator.add("2,3,4,6,2,10")).toBe(27)
+    expect(stringCalculator.calculate("2,3,4,6,2,10")).toBe(27)
   })
 
   test("should return the sum of multiple numbers with new line", () => {
-    expect(stringCalculator.add("2\n3,4,6,2,10")).toBe(27)
+    expect(stringCalculator.calculate("2\n3,4,6,2,10")).toBe(27)
+  })
+
+  test("should return the sum of multiple numbers with custom delimiter", () => {
+    expect(stringCalculator.calculate("//;\n2;3;4;6;2;10")).toBe(27)
   })
 })
